0fbc48cf5e0328fe4e1d22804f125eafc8af1599
galimova_aida_11-808 <aidagalimova03@gmail.com>
2019-05-14 16:08:53 +0300

    kr2

--- 2019/misc/kr2/Program.cs
+++ 2019/misc/kr2/Program.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
 using System.Web;
-using System.Net.Http;
-namespace кр
+using System.Net;
+namespace kr
 {
     class MyClass
     {
@@ -19,42 +19,25 @@ namespace кр
     }
     class Program
     {
-        public static async void Solve()
-        {
-            string baseUrl = "https://jsonplaceholder.typicode.com/comments";
-            try
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    using (HttpResponseMessage res = await client.GetAsync(baseUrl))
-                    {
-                        using (HttpContent content = res.Content)
-                        {
-                            var data = await content.ReadAsStringAsync();
-                            List<MyClass> lst = JsonConvert.DeserializeObject<List<MyClass>>(data);
-
-                            int ans = 0;
-                            for (int i = 0; i < lst.Count; i++)
-                            {
-                                if (i % 2 == 0)
-                                {
-                                    ans += lst[i].body.Length;
-                                }
-                            }
-                            Console.WriteLine(ans);
-                        }
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine("Exception Hit------------");
-                Console.WriteLine(exception);
-            }
-        }
         static void Main(string[] args)
         {
             Solve();
+        }
+
+        public static void Solve()
+        {
+            string HtmlText = string.Empty;
+            string url = "https://jsonplaceholder.typicode.com/comments";
+            HttpWebRequest myHttwebrequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse myHttpWebresponse = (HttpWebResponse)myHttwebrequest.GetResponse();
+            StreamReader strm = new StreamReader(myHttpWebresponse.GetResponseStream());
+            HtmlText = strm.ReadToEnd();
+
+            List<MyClass> lst = JsonConvert.DeserializeObject<List<MyClass>>(HtmlText);
+
+            int ans = (lst.Where((item, index) => index % 2 == 0)).Sum(a => a.body.Length);
+
+            Console.WriteLine(ans);
         }
     }
 }
